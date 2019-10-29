d053b0047a8aff61c43e7abc7b71cf663cf6d02a
christma <liuchuanmumumu@gmail.com>
2019-10-29 22:39:02 +0800

    add Stack

--- src/Stack/Main.java
+++ src/Stack/Main.java
@@ -1,5 +1,7 @@
 package Stack;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
         ArrayStack<Integer> stack = new ArrayStack<Integer>();
@@ -9,5 +11,33 @@ public class Main {
         }
         stack.pop();
         System.out.println(stack);
+    }
+    // leetcode 20
+    class Solution {
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(' || c == '[' || c == '{') {
+                    stack.push(c);
+                } else {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+                    char topChar = stack.pop();
+                    if (c == ')' && topChar != '(') {
+                        return false;
+                    }
+                    if (c == '}' && topChar != '{') {
+                        return false;
+                    }
+                    if (c == ']' && topChar != '[') {
+                        return false;
+                    }
+
+                }
+            }
+            return stack.isEmpty();
+        }
     }
 }
