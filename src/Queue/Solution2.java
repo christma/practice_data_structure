bac10ce228b2a18e3f1765678c4ef42eb3c84e24
christma <liuchuanmumumu@gmail.com>
2019-11-08 06:32:06 +0800

    add solution2

---
+++ src/Queue/Solution2.java
@@ -0,0 +1,31 @@
+package Queue;
+
+public class Solution2 {
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode res = removeElements(head.next, val);
+        if (head.val == val) {
+            return res;
+        } else {
+            head.next = res;
+            return head;
+        }
+
+    }
+
+}
